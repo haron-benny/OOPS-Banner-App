c9d5aa649b2a0f1504a19267020a7b320101bae3
haron-benny <hb2863@srmist.edu.in>
2026-03-03 21:34:01 +0530

    Coded UC2 to print banner using print statement

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,23 @@
+/**
+ * OOPSBannerApp UC2 - Banner Display Application
+ *
+ * This class demonstrates the creation of a visual banner displaying the word "OOPS"
+ * using ASCII art composed of asterisks (*) and spaces.
+ *
+ * @author Haron
+ * @version 2.0
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****    *****    ******   *****  ");
+        System.out.println(" *     *  *     *  *      * *     * ");
+        System.out.println(" *     *  *     *  *      * *       ");
+        System.out.println(" *     *  *     *  ******    *****  ");
+        System.out.println(" *     *  *     *  *              * ");
+        System.out.println(" *     *  *     *  *         *     * ");
+        System.out.println("  *****    *****   *          *****  ");
+
     }
 }
