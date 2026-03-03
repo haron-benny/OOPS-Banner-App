25c5d5413932e96d3508d5ff761405dc9dc1bdf8
haron-benny <hb2863@srmist.edu.in>
2026-03-03 21:57:41 +0530

    UC3: Rafractor banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,26 @@
-/**
- * OOPSBannerApp UC2 - Banner Display Application
- *
- * This class demonstrates the creation of a visual banner displaying the word "OOPS"
- * using ASCII art composed of asterisks (*) and spaces.
- *
- * @author Haron
- * @version 2.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("  *****    *****    ******   *****  ");
-        System.out.println(" *     *  *     *  *      * *     * ");
-        System.out.println(" *     *  *     *  *      * *       ");
-        System.out.println(" *     *  *     *  ******    *****  ");
-        System.out.println(" *     *  *     *  *              * ");
-        System.out.println(" *     *  *     *  *         *     * ");
-        System.out.println("  *****    *****   *          *****  ");
+        System.out.println(String.join(" ",
+                "  *****", "   *****", "   ******", "   *****"));
 
+        System.out.println(String.join(" ",
+                " *     *", " *     *", " *      *", " *     *"));
+
+        System.out.println(String.join(" ",
+                " *     *", " *     *", " *      *", " *"));
+
+        System.out.println(String.join(" ",
+                " *     *", " *     *", " ******", "   *****"));
+
+        System.out.println(String.join(" ",
+                " *     *", " *     *", " *", "        *"));
+
+        System.out.println(String.join(" ",
+                " *     *", " *     *", " *", " *     *"));
+
+        System.out.println(String.join(" ",
+                "  *****", "   *****", " *", "   *****"));
     }
 }
