f5c945e535b1052eade52f4152d4bba8dd4d52b8
haron-benny <hb2863@srmist.edu.in>
2026-03-03 16:50:20 +0530

    Coded UC1 to print OOPS to console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,28 +1,5 @@
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-
-        // Inline array declaration and initialization
-        String[] lines = {
-            String.join("  ",
-                "  *****  ", "  *****  ", " ******  ", "  *****  "),
-            String.join("  ",
-                " *     * ", " *     * ", " *      *", " *     * "),
-            String.join("  ",
-                " *     * ", " *     * ", " *      *", " *       "),
-            String.join("  ",
-                " *     * ", " *     * ", " ******  ", "  *****  "),
-            String.join("  ",
-                " *     * ", " *     * ", " *       ", "       * "),
-            String.join("  ",
-                " *     * ", " *     * ", " *       ", " *     * "),
-            String.join("  ",
-                "  *****  ", "  *****  ", " *       ", "  *****  ")
-        };
-
-        // Loop to print banner
-        for (String line : lines) {
-            System.out.println(line);
-        }
+        System.out.println("OOPS");
     }
 }
